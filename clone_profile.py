03916ba45f1fdd771b1b39d1c636d2a5bad282b1
gosen <agusmdev@gmail.com>
2019-01-07 02:21:37 -0300

    Added optionals arguments

--- clone_profile.py
+++ clone_profile.py
@@ -5,6 +5,7 @@ import time
 import re
 from access import *
 from random import randint
+import argparse
 import threading
 
 
@@ -22,22 +23,26 @@ class Api(object):
 
     def retrieve_tweets(self, user, count=10):
         try:
-            # print(len(list(tweepy.Cursor(self.api.user_timeline, id=user.id).items())))
             return self.api.user_timeline(screen_name=user, count=20)
         except Exception as e:
             raise e
 
+    def post_tweet(self, tweet_text):
+        try:
+            self.api.update_status(tweet_text)
+            print("Successfully posted.")
+        except tweepy.TweepError as e:
+            print(e.reason)
+
     def clone_last_tweets(self, user_clone):
         tweets = self.retrieve_tweets(user_clone)[::-1]
+        self.post_tweet("cloning.. @{}".format(user_clone))
+
         for tweet in tweets:
             # Print tweet:
             print(tweet.text)
             if tweet.text[0] != "R" and tweet.text[1] != "T":
-                try:
-                    self.api.update_status(tweet.text)
-                    print("Successfully posted.")
-                except tweepy.TweepError as e:
-                    print(e.reason)
+                self.post_tweet(tweet.text)
         return True
 
     def delete_tweets(self):
@@ -49,12 +54,26 @@ class Api(object):
             except Exception as e:
                 print(e)
 
+    def print_tweets(self, tweets):
+        for i in tweets:
+            print(i.text)
 
-bot = Api()
 
-secs = 1
-user = bot.get_user(input("Ingrese un usuario: "))
-tweets = bot.retrieve_tweets(user)
-for i in tweets:
-    print(i.text)
-# threading.Thread(target=bot.clone_last_tweets(user.screen_name))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--print", help="print tweets from the give profile",
+                        action="store_true")
+    parser.add_argument("-c", "--clone", help="clone some tweets from the give profile",
+                        action="store_true")
+    args = parser.parse_args()
+
+    bot = Api()
+    secs = 1
+    user = input("Ingrese un usuario: ")
+
+    if args.print:
+        bot.print_tweets(bot.retrieve_tweets(user))
+    elif args.clone:
+        bot.clone_last_tweets(user)
+    else:
+        print("LA CONCHA DE TU MADRE")
